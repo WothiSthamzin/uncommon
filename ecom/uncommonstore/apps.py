c2fe789537eb916f08bca5999b2e19b68cff49f4
“WothiSthamzin” <wothingena@hotmail.com>
2024-10-23 14:36:11 +0200

    Add docstrings to apps.py for clarity

--- ecom/uncommonstore/apps.py
+++ ecom/uncommonstore/apps.py
@@ -2,5 +2,16 @@ from django.apps import AppConfig
 
 
 class UncommonstoreConfig(AppConfig):
+    """
+    Configuration for the Uncommon Store application.
+
+    This class configures the Uncommon Store app and sets the default
+    auto field type for primary keys.
+
+    Attributes:
+        default_auto_field (str): The type of auto field to use for primary keys.
+        name (str): The name of the application.
+    """
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'uncommonstore'
