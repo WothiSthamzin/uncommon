dfe7733e69eab4d546d6587eac1549c2ed6f3457
“WothiSthamzin” <wothingena@hotmail.com>
2024-10-23 15:09:10 +0200

    Add docstrings to cart.py for improved documentation

--- ecom/cart/cart.py
+++ ecom/cart/cart.py
@@ -1,47 +1,62 @@
 from uncommonstore.models import Product
 
 class Cart():
-    # Request : Anytime a user views the 'Cart' page
+    """
+    A shopping cart class to manage the user's cart session.
+
+    This class is responsible for storing cart items, calculating totals,
+    and managing the cart's state within the user's session.
+
+    Attributes:
+        session (Session): The session object associated with the user's request.
+        cart (dict): A dictionary storing product IDs and their quantities in the cart.
+    """
     def __init__(self, request):
+        """
+        Initializes the Cart instance.
+
+        Args:
+            request (HttpRequest): The request object containing session data.
+        """
         self.session = request.session
-        # If it exists, get current session key 
         cart = self.session.get('session_key')
 
-        # If user is new, create a session key
         if 'session_key' not in request.session:
             cart = self.session['session_key'] = {}
-
-        # Making sure 'cart' is available throught the site
         self.cart = cart
 
-    # Add to cart function
     def add(self, product, quantity):
+        """
+        Adds a product to the cart.
+
+        Args:
+            product (Product): The product to add.
+            quantity (int): The quantity of the product to add.
+        """
         product_id = str(product.id)
         product_qty = str(quantity)
-        # Logic
         if product_id in self.cart:
             pass
         else:
-            # self.cart[product_id] = {'price': str(product.price)}
             self.cart[product_id] = int(product_qty)
 
         self.session.modified = True
 
     def cart_total(self):
-        # Getting product IDs
+        """
+        Calculates the total price of items in the cart.
+
+        Returns:
+            float: The total price of all products in the cart.
+        """
         product_ids = self.cart.keys()
-        # Calling up keys in products database
         products = Product.objects.filter(id__in=product_ids)
-        # get quantities
         quantities = self.cart
-        # Start count from zero
         total = 0
         for key, value in quantities.items():
-            # Converting key string into integer for calculations
             key = int(key)
             for product in products:
                 if product.id == key:
-                    # For products on sale
                     if product.is_sale:
                         total = total + (product.sale_price * value)
                     else:
@@ -49,30 +64,51 @@ class Cart():
 
         return total
 
-    # Getting length, to count items in cart
     def __len__(self):
+        """
+        Returns the number of items in the cart.
+
+        Returns:
+            int: The total number of items in the cart.
+        """
         return len(self.cart)
     
-    # Getting products
+
     def get_prods(self):
-        # Product IDs stored in a dictionary. Calling relevant products in dictionary 
+        """
+        Retrieves the products currently in the cart.
+
+        Returns:
+             A queryset of Product objects in the cart.
+        """
         product_ids = self.cart.keys()
-        # Use IDs to look up products in database model
         products = Product.objects.filter(id__in=product_ids)
-        # Return those looked up products
         return products
     
-    # Getting quantities ordered
     def get_quants(self):
+        """
+        Retrieves the quantities of products in the cart.
+
+        Returns:
+            dict: A dictionary of product IDs and their corresponding quantities.
+        """
         quantities = self.cart
         return quantities
     
     def update(self, product, quantity):
+        """
+        Updates the quantity of a specified product in the cart.
+
+        Args:
+            product (str): The product ID to update.
+            quantity (int): The new quantity for the product.
+
+        Returns:
+            dict: The updated cart.
+        """
         product_id = str(product)
         product_qty = int(quantity)
-        # Get Cart
         usercart = self.cart
-        # Update Dictionary/cart
         usercart[product_id] = product_qty
 
         self.session.modified = True
@@ -81,8 +117,13 @@ class Cart():
         return thing
 
     def delete(self, product):
+        """
+        Removes a specified product from the cart.
+
+        Args:
+            product (str): The product ID to remove.
+        """
         product_id = str(product)
-        # Delete from Dictionary/cart
         if product_id in self.cart:
             del self.cart[product_id]
 
