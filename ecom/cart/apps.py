a136d87773d9dcd7b3b328c42de0ed5955b8586d
“WothiSthamzin” <wothingena@hotmail.com>
2024-10-23 14:52:29 +0200

    Add docstrings to apps.py in cart folder for clarity

--- ecom/cart/apps.py
+++ ecom/cart/apps.py
@@ -2,5 +2,15 @@ from django.apps import AppConfig
 
 
 class CartConfig(AppConfig):
+    """
+    Configuration for the Cart application.
+
+    This class configures the Cart app and sets the default
+    auto field type for primary keys.
+
+    Attributes:
+        default_auto_field (str): The type of auto field to use for primary keys.
+        name (str): The name of the application.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cart'
